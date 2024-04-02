696958eb8721db678305d3f19e937779daaadc08
JoseAGomezBravo <04josegomez2022@gmail.com>
2024-04-01 19:49:19 -0700

    commit

---
+++ COMP003B.Assignment2/Controllers/InfoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment2.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET "InfoController/Hobbies"
+        public IActionResult Hobbies()
+        {
+            return View();
+        }
+
+        // GET "InfoController/About"
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
--- COMP003B.Assignment2/Program.cs
+++ COMP003B.Assignment2/Program.cs
@@ -7,20 +7,17 @@ namespace COMP003B.Assignment2
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddRazorPages();
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            // middleware
-
             // added Welcome page middleware at /Welcome
             app.UseWelcomePage("/Welcome");
             // added HTTP error code middleware
@@ -32,7 +29,9 @@ namespace COMP003B.Assignment2
 
             app.UseAuthorization();
 
-            app.MapRazorPages();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
